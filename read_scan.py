56ca6ff938f151c09c13327a2123a1e13d98c4ba
Matteo Abis <gmatteo..abis@gmail.com>
2014-07-29 17:20:08 +0200

    scan shows as BW image

--- read_scan.py
+++ read_scan.py
@@ -14,9 +14,37 @@ parser.add_argument(
     help="folder where the raw files from the scan are saved"
 )
 
+parser.add_argument(
+    "--min_pixel",
+    nargs='?',
+    default=1250,
+    type=int,
+    help="first pixel"
+)
+
+parser.add_argument(
+    "--max_pixel",
+    nargs='?',
+    default=2560,
+    type=int,
+    help="last pixel"
+)
+
 if __name__ == '__main__':
     args = parser.parse_args()
-    folder = args.folder
+    folder = args.folder[0]
     file_names = glob.glob(os.path.join(folder, "*.raw"))
-    print(file_names)
-    #datasets = np.
+    #print(file_names)
+    datasets = np.vstack([np.loadtxt(file_name, dtype=np.int)[
+        args.min_pixel:args.max_pixel, 1]
+        for file_name in file_names])
+    print(datasets)
+    plt.figure()
+    plt.imshow(datasets, interpolation="none")
+    plt.tight_layout()
+    plt.figure()
+    integrals = [np.sum(dataset) for dataset in datasets]
+    plt.plot(integrals)
+    plt.show()
+    plt.ion()
+    input()
