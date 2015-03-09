73d3d726a6663fbacf5302aa02d7f3b69998cde2
Matteo Abis <gmatteo..abis@gmail.com>
2015-03-09 15:47:30 +0100

    add pilatus reader

---
+++ read_pilatus.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+
+import argparse
+
+parser = argparse.ArgumentParser(
+    __doc__,
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument(
+    "file",
+    nargs=1,
+    help="file"
+)
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    file_name = args.file[0]
+    image = np.fromfile(file_name, dtype=np.int32).reshape((619, 487))
+    plt.figure()
+    im_show = plt.imshow(image, interpolation="none", aspect='auto')
+    limits = stats.mstats.mquantiles(image, prob=[0.02, 0.98])
+    im_show.set_clim(*limits)
+    print(limits)
+    plt.tight_layout()
+    #plt.figure()
+    #integrals = [np.sum(dataset) for dataset in datasets]
+    #plt.plot(integrals)
+    plt.show()
+    plt.ion()
+    input()
