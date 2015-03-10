706943fe87d682924bbf296c607834c4b16dd192
Matteo Abis <gmatteo..abis@gmail.com>
2015-03-10 10:45:22 +0100

    print integrals in pilatus

--- read_pilatus.py
+++ read_pilatus.py
@@ -17,11 +17,13 @@ if __name__ == '__main__':
     args = parser.parse_args()
     file_name = args.file[0]
     image = np.fromfile(file_name, dtype=np.int32).reshape((619, 487))
+    print(image)
     plt.figure()
     im_show = plt.imshow(image, interpolation="none", aspect='auto')
     limits = stats.mstats.mquantiles(image, prob=[0.02, 0.98])
     im_show.set_clim(*limits)
     print(limits)
+    print(np.sum(image[image >=0]))
     plt.tight_layout()
     #plt.figure()
     #integrals = [np.sum(dataset) for dataset in datasets]
